3f62667ea5e3425ba67a5f668e88e2441a565884
rockets-in-brighton <40665247+rockets-in-brighton@users.noreply.github.com>
2018-06-29 22:52:39 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# config.py
+
+# these are GPIO Pins
+CONFIG = {
+    'Left.Button': 2,
+    'Right.Button': 17,
+    'Left.Motor.A': 14,
+    'Left.Motor.B': 15,
+    'Right.Motor.A': 23,
+    'Right.Motor.B': 24,
+
+    'PWM.Frequency': 100,
+
+    'Sleep.Time.Seconds': 50 / 1000,
+
+    'Speed.Max': 100,
+    'Speed.Increment': 5
+}
